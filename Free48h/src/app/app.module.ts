f51e7b19895605969591b37e6dda8dbeca837d20
jeremy bussy <bussyjeremy@gmail.com>
2023-03-23 15:01:53 +0100

    Ajout routes

--- Free48h/src/app/app.module.ts
+++ Free48h/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TicketComponent } from './pages/ticket/ticket.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MyPageComponent } from './pages/my-page/my-page.component';
+import { MiniatureTicketComponent } from './composants/miniature-ticket/miniature-ticket.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TicketComponent,
+    LoginComponent,
+    MyPageComponent,
+    MiniatureTicketComponent
   ],
   imports: [
     BrowserModule,
---
+++ Free48h/src/app/composants/miniature-ticket/miniature-ticket.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-miniature-ticket',
+  templateUrl: './miniature-ticket.component.html',
+  styleUrls: ['./miniature-ticket.component.css']
+})
+export class MiniatureTicketComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
